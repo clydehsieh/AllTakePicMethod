ac143c3a693e95265c5b7e7e953b139b99699b5b
clydehsieh <clydehsieh0601@gmail.com>
2015-05-19 09:42:35 +0800

    Initial Commit

---
+++ AllTakePicMethod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AllTakePicMethod
+//
+//  Created by Chin-Hui Hsieh  on 5/19/15.
+//  Copyright (c) 2015 Chin-Hui Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
